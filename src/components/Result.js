f158d8d3518741612a16dcca382e9643ec2a2e01
noellepavlovic <31223987+noellepavlovic@users.noreply.github.com>
2017-11-27 14:52:41 -0500

    first attempt results

--- src/components/Result.js
+++ src/components/Result.js
@@ -5,10 +5,63 @@ import '../App.css';
 class Result extends Component {
   render() {
     return (
-    <div className="container">
-        <h1> Results</h1>
-    </div>
-    );
+ 
+      (!this.state.movie) ?
+        (<div className="container">
+          <div className="row">
+            <div className="col-xs-12">
+              <h1> Please select a movie genre from the drop-down above</h1>
+            </div>
+          </div>
+        </div>)
+        :
+        (<div className="container">
+          <div className="row">
+            <div className="col-xs-12">
+              <h1> Results</h1>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-xs-4">
+              <img className="detailsImg" src={"https://image.tmdb.org/t/p/w300_and_h450_bestv2/" + this.movie.poster_path} alt="Image of movie Poster" />
+            </div>
+            <div className="col-xs-8">
+              <div className="row">
+                <div className="col-xs-2 descHeading">
+                  Title:
+            </div>
+                <div className="col-xs-10">
+                  {this.props.movie.title}
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-xs-2 descHeading">
+                  Rating:
+            </div>
+                <div className="col-xs-10">
+                  {this.props.movie.vote_avg}
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-xs-2 descHeading">
+                  Release Date:
+            </div>
+                <div className="col-xs-10">
+                  {this.props.movie.release_date}
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-xs-2 descHeading">
+                  Genre:
+            </div>
+                <div className="col-xs-10">
+                  {this.props.movie.overview}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>)
+    )
   }
 }
 
